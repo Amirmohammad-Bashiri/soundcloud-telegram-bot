e02085da3448074d6583986f06abeb1b6581ce5b
Amirmohammad-Bashiri <amirmohamad.bashiri@gmail.com>
2021-10-01 10:48:12 +0330

    Added base url path and removed playlist url checker

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const fs = require("fs");
-const scdl = require("soundcloud-downloader").default;
 const dotenv = require("dotenv");
 const TelegramBot = require("node-telegram-bot-api");
 
 const singleDownload = require("./singleDownload");
-const playlistDownload = require("./playlistDownload");
 // Utils
 const userDetailsLogger = require("./utils/userDetailsLogger");
 const urlExtractor = require("./utils/urlExtractor");
@@ -21,6 +19,7 @@ const token = process.env.BOT_TOKEN;
 
 const bot = new TelegramBot(token, {
   polling: true,
+  baseApiUrl: "http://localhost:5000",
 });
 
 bot.on("message", msg => {
@@ -33,16 +32,13 @@ bot.on("message", msg => {
     if (msg.text !== "/start") {
       userDetailsLogger(bot, chatId);
 
-      const SOUNDCLOUD_URL = urlExtractor(msg);
+      const SOUNDCLOUD_URL = urlExtractor(msg, bot, chatId);
+      console.log(SOUNDCLOUD_URL);
 
-      if (scdl.isPlaylistURL(SOUNDCLOUD_URL)) {
-        playlistDownload(SOUNDCLOUD_URL, bot, chatId, fileOptions);
-      } else {
-        singleDownload(SOUNDCLOUD_URL, bot, chatId, fileOptions);
-      }
+      singleDownload(SOUNDCLOUD_URL, bot, chatId, fileOptions);
     }
   } catch (error) {
     console.error(error);
-    bot.sendMessage(chatId, "Something went wrong, Please try again later");
+    bot.sendMessage(chatId, "Please enter a valid Soundcloud url.");
   }
 });
